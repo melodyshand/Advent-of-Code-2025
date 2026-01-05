235d707a83f4cbcd9de4255acbeeeb6f09c0c5af
Melody Shand <melodyshand@gmail.com>
2026-01-05 05:34:35 +0000

    Day 4 pt2 solution

--- day4.py
+++ day4.py
@@ -1,27 +1,20 @@
 with open("/Users/melody.shand/Documents/MS/Python/repos/Advent-of-Code-2025/day4_input.txt", "r") as file:
     # with open("/Users/melody.shand/Documents/MS/Python/repos/Advent-of-Code-2025/day4_input_sample.txt", "r") as file:
-    inputData = [line.split() for line in file]
+    # inputData = file.readlines().split()
+    inputData = [line.strip() for line in file]  # each row is a string
 
-
+# Directions:
 # NW N NE
 #  W @ E
 # SW S SE
-# NW = line -1, coord -1
-# N = line - 1, coord 0
-# NE = line -1, coord +1
-# W = line 0, coord -1
-# E = line 0, coord +1
-# SW = line +1, coord -1
-# S = line +1, coord 0
-# SE = line +1, coord +1
+
+# Check if a cell contains a paper roll
 
 
-# for each @ symbol we need to find the above corordinates
 def isPaperRoll(item):
-    if item == "@":
-        return True
-    else:
-        return False
+    return item == "@"
+
+# Check if coordinates are out of bounds
 
 
 def isOutOfBounds(currentitemIndex, currentItemLine, inputData, lineIterator, itemIterator, lines, items):
@@ -35,58 +28,48 @@ def isOutOfBounds(currentitemIndex, currentItemLine, inputData, lineIterator, it
         return True
     else:
         return False
-     # need to cater to literal edge cases
+
+# Count neighbouring rolls around a cell
 
 
 def findNeighbourItems(currentitemIndex, currentItemLine, inputData, lines, items):
     rolls = 0
     lineIterator = -1
-    itemIterator = -1
 
     while lineIterator < 2:
-        itemIterator = -1   # RESET HERE
+        itemIterator = -1
         while itemIterator < 2:
+            # skip the center cell
             if lineIterator == 0 and itemIterator == 0:
                 itemIterator += 1
                 continue
 
-            elif isOutOfBounds(currentitemIndex, currentItemLine, inputData, lineIterator, itemIterator, lines, items):
-                rolls += 0
-            elif isPaperRoll(inputData[currentItemLine + lineIterator][0][currentitemIndex + itemIterator]):
-                rolls += 1
+            # skip out-of-bounds positions
+            if not isOutOfBounds(currentitemIndex, currentItemLine, inputData, lineIterator, itemIterator, lines, items):
+                if isPaperRoll(inputData[currentItemLine + lineIterator][currentitemIndex + itemIterator]):
+                    rolls += 1
             itemIterator += 1
         lineIterator += 1
 
+    # accessible if fewer than 4 neighbours
     if rolls < 4:
         return 1
     else:
         return 0
 
-    # coordinates["NW"] = inputData[northLine][currentitemIndex - 1]
-    # coordinates["N"] = inputData[northLine][currentitemIndex]
-    # coordinates["NE"] = inputData[northLine][currentitemIndex + 1]
-    # coordinates["W"] = inputData[currentItemLine][currentitemIndex - 1]
-    # coordinates["E"] = inputData[currentItemLine][currentitemIndex + 1]
-    # coordinates["SW"] = inputData[southline][currentitemIndex - 1]
-    # coordinates["S"] = inputData[southline][currentitemIndex]
-    # coordinates["SE"] = inputData[southline][currentitemIndex + 1]
-
-
-inputData
+# Count all accessible rolls
 
 
 def findSurroundingRolls(inputData):
     accessibleRolls = 0
     lines = len(inputData)
-    items = len(inputData[0][0])
-    currentitemLine = 0
-    currentItemIndex = 0
+    items = len(inputData[0])  # number of columns
 
+    currentitemLine = 0
     while currentitemLine < lines:
-        lineData = list(inputData[currentitemLine])
-        currentItemIndex = 0   # RESET HERE
-        while currentItemIndex < len(lineData[0]):
-            if isPaperRoll(inputData[currentitemLine][0][currentItemIndex]):
+        currentItemIndex = 0
+        while currentItemIndex < items:
+            if isPaperRoll(inputData[currentitemLine][currentItemIndex]):
                 accessibleRolls += findNeighbourItems(
                     currentItemIndex, currentitemLine, inputData, lines, items)
             currentItemIndex += 1
@@ -95,14 +78,90 @@ def findSurroundingRolls(inputData):
     print("Day 4 PART 1 answer: ", accessibleRolls)
 
 
+# Run the calculation
 findSurroundingRolls(inputData)
 
 
-# for each line
-# for each coordinate
-# norths only possible if line not 1
-# Wests only possible if coord not 0
-# easts only possible if coord not -1
-# souths only possible if line not end
+# part 2
+# find accessible rows
+# remove/replace)?) in 1 go
+# repest
 
-# find those values and if they are @ , increment
+
+def findCoordsAccessibleItems(currentitemIndex, currentItemLine, inputData, lines, items):
+    rolls = 0
+    lineIterator = -1
+
+    while lineIterator < 2:
+        itemIterator = -1
+
+        while itemIterator < 2:
+            # skip the center cell
+            if lineIterator == 0 and itemIterator == 0:
+                itemIterator += 1
+                continue
+
+            # skip out-of-bounds positions
+            if not isOutOfBounds(currentitemIndex, currentItemLine, inputData, lineIterator, itemIterator, lines, items):
+                if isPaperRoll(inputData[currentItemLine + lineIterator][currentitemIndex + itemIterator]):
+                    rolls += 1
+
+            itemIterator += 1
+        lineIterator += 1
+
+    # accessible if fewer than 4 neighbours
+    if rolls < 4:
+        return (currentItemLine, currentitemIndex)  # accessible
+    else:
+        return None
+
+
+# def numberOfAccessibleRolls(accessibleRolls):
+#     numOfRolls = accessibleRolls.keys()
+#     return len(numOfRolls)
+
+
+def removeRolls(inputData, rollsToRemove):
+    for (r, c) in rollsToRemove:
+        row = list(inputData[r])
+        row[c] = '.'
+        inputData[r] = ''.join(row)
+
+
+def countNumberOfRemovals(accessibleRolls):
+    return len(accessibleRolls)
+
+
+def pt2(inputData):
+    lines = len(inputData)
+    items = len(inputData[0])  # number of columns
+    totalRemoved = 0
+    currentitemLine = 0
+
+    while True:
+        accessibleRolls = []
+        currentitemLine = 0
+
+        while currentitemLine < lines:
+            currentItemIndex = 0
+            while currentItemIndex < items:
+                if isPaperRoll(inputData[currentitemLine][currentItemIndex]):
+                    coords = findCoordsAccessibleItems(
+                        currentItemIndex, currentitemLine, inputData, lines, items)
+
+                    if coords is not None:
+                        accessibleRolls.append(coords)
+
+                currentItemIndex += 1
+            currentitemLine += 1
+
+        if not accessibleRolls:
+            break
+
+        removeRolls(inputData, accessibleRolls)
+        totalRemoved += len(accessibleRolls)
+
+    print("Day 4 PART 2 answer: ", totalRemoved)
+
+
+pt2(inputData)
